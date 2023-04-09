4ca68020ce6fbff9b294e817b3e481d942b6674b
Влад Черторыгин <vladcertorygin@MacBook-Air.local>
2023-04-09 12:11:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("--------");
+
+        for (int i = 20; i < 40; i++) {
+            System.out.println(i);
+        }
+    }
+}
